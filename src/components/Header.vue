1db3c6890540431967fafad8224ebe9d55b8160b
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2021-11-10 13:46:57 +0700

    First Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="text-center bg-blue-600 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3"> <i class="fa fa-viruses"></i> Covid-19 Tracker</div>
+
+    <p>API by <a href="https://covid19api.com" class="text-blue-300" target="blank">covid19api.com</a></p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+}
+</script>
